31309a5cb0328fe553d4e7ecea91a2be5e4e5e67
Bruno Lins <bruno4128@hotmail.com>
2023-06-22 03:11:54 -0300

    Arquivos

---
+++ mysite/enquetes/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Choice, Question
+from django.http import Http404
+
+
+
+
+# Create your views here.
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pubdate")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "results.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "enquetes/detail.html",{"question": question, "error_message": "Você não selecionou uma alternativa.",},)
+    else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse("enquetes:results", args=(question.id,)))
