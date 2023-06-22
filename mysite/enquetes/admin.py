5afac8720c07b7d1a3d0b5ba007e8bb831a50b28
Bruno Lins Dos Santos Viana <20221014040010@ifrn.local>
2023-06-22 09:37:34 -0300

    att

--- mysite/enquetes/admin.py
+++ mysite/enquetes/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pubdate']}),
+    ]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['choice_text']}),
+        ('Votos', {'fields': ['votes']}),
+    ]
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- mysite/simulado/models.py
+++ mysite/simulado/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Questao(models.Model):
+    questao_texto = models.CharField(max_length=200)
+    data_pub = models.DateTimeField("data publicada")
