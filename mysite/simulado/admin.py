cf0bb7aa05dd1e2e73199100bb32273b54381a31
Bruno Lins <bruno4128@hotmail.com>
2023-06-26 21:29:29 -0300

    classes models e admin

--- mysite/simulado/admin.py
+++ mysite/simulado/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Simulado, Questao, Alternativa
 
 # Register your models here.
+admin.site.site_header = 'Administração Simulado'
+
+class AlternativaInLine(admin.TabularInline):
+    model = Alternativa
+    extra = 2
+
+
+
+
+class QuestaoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['questao_texto']}),
+        ('Date information', {'fields': ['questao_data']}),
+        ('Pontuação', {'fields': ['pontuacao']}),
+    ]
+    inlines = [AlternativaInLine]
+    list_display = ('questao_texto', 'id', 'questao_data')
+    list_filter = ['questao_data']
+    search_fields = ['questao_texto']
+
+
+
+admin.site.register(Questao, QuestaoAdmin)
--- mysite/simulado/models.py
+++ mysite/simulado/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Simulado(models.Model):
+    simulado_nome = models.CharField(max_length=100)
+    simulado_data = models.DateTimeField("data criada")
+    tema = models.CharField(max_length=200)
+
+
 class Questao(models.Model):
     questao_texto = models.CharField(max_length=200)
-    data_pub = models.DateTimeField("data publicada")
+    questao_data = models.DateTimeField("data publicada")
+    pontuacao = models.PositiveIntegerField(default=0, max_length=3)
+    simulado = models.ForeignKey(Simulado, on_delete=models.SET_NULL, null=True)
+
+
+class Alternativa(models.Model):
+    alternativa_texto = models.CharField(max_length=200)
+    questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
+
+
+
+#  COISAS PARA FAZER
+#  Utilizar validador para delimitar pontuação da questão
+#  Conseguir selecionar a questão para um simulado durante sua criação
+#  Aprender a determinar qual alternativa será a correta
+#
+#
+#
